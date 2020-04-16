f6d357525e81d34f0baf3967d46f76940fef60f6
BritishPandaChick <sbartley1080@gmail.com>
2020-04-16 00:53:59 -0400

    Add socket.io. Add code to setup environment

--- public/client.js
+++ public/client.js
@@ -10,6 +10,7 @@ $( document ).ready(function() {
     return false; // prevent form submit from refreshing page
   });
   
-  
+  /* Global io */
+  var socket = io();
   
 });
--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const cookieParser= require('cookie-parser')
 const app         = express();
 const http        = require('http').Server(app);
 const sessionStore= new session.MemoryStore();
+const io = require('socket.io')(http);
+const cors = require('cors');
 
-
+app.use(cors());
 fccTesting(app); //For FCC testing purposes
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -41,7 +43,9 @@ mongo.connect(process.env.DATABASE, (err, db) => {
 
   
     //start socket.io code  
-
+    io.on('connection', socket => {
+      console.log('A user has connected');
+    });
   
 
     //end socket.io code
